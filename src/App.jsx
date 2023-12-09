16b5fdc3909e150d25a1e5666bb9ef357e7f7486
Jerry Huen <jellybouncejerry@gmail.com>
2023-12-09 11:52:23 +0800

    formatting change

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import SingleLocation from "./Components/SingleLocation.jsx"
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SingleLocation from "./Components/SingleLocation.jsx";
 function App() {
   return (
     <div className="App">
-    <BrowserRouter>
-    <Routes>
-      <Route path="/location/:locId" element={<SingleLocation/>}/>
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/location/:locId" element={<SingleLocation />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
