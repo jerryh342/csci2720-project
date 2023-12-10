4b939e799f5777bf6077efd9093db3f1151b5dce
Jerry Huen <jellybouncejerry@gmail.com>
2023-12-10 15:19:59 +0800

    added routing for nomatch

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SingleLocation from "./Components/SingleLocation.jsx";
+import NoMatch from "./Components/NoMatch.jsx";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/location/:locId" element={<SingleLocation />} />
+          <Route path="*" element={<NoMatch />} />
         </Routes>
       </BrowserRouter>
     </div>
