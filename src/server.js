e551cbbfe2d8220114f231d2c9468d810263a1fe
longchingkwok331 <138690628+longchingkwok331@users.noreply.github.com>
2023-12-11 00:12:14 +0800

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -1,85 +1,133 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
-const EventSchema = require("./schemas.js").EventSchema;
-const VenueSchema = require("./schemas.js").VenueSchema;
-const CommentSchema = require("./schemas.js").CommentSchema;
+const passport = require("passport");
+const passportLocal = require("passport-local").Strategy;
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+// const MongoStore = require('connect-mongo');
+
+const LoginModel = require('./models/login')
+
 const app = express();
-app.use(cors());
-app.use(express.json());
-mongoose.connect("mongodb+srv://admin:admin@csci2720proj.pqtx7pq.mongodb.net/?retryWrites=true&w=majority"); // my cloud deployment
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "http://localhost:3000", // <-- location of the react app were connecting to
+    credentials: true,
+  })
+);
+app.use(
+  session({
+    secret: "secretcode",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(cookieParser("secretcode"));
+app.use(passport.initialize());
+app.use(passport.session());
+require("./passportConfig")(passport);
+
+
+const Schema = mongoose.Schema;
+mongoose.connect('mongodb://127.0.0.1:27017/project'); // put your own database link here
+
 const db = mongoose.connection;
 // Upon connection failure
-db.on("error", console.error.bind(console, "Connection error:"));
+db.on('error', console.error.bind(console, 'Connection error:'));
 // Upon opening the database successfully
-db.once("open", function () {
-  console.log("Connection is open...");
-});
+db.once('open', function () {
+    console.log("Connection is open... success");
 
-const Event = mongoose.model("Event", EventSchema);
-const Venue = mongoose.model("Venue", VenueSchema);
-const Comment = mongoose.model("Comment", CommentSchema);
-//get venue details
-app.get("/venue/:venueId", (req, res) => {
-  res.setHeader("Content-Type", "application/json");
-  Venue.findOne({ venueId: req.params.venueId })
-    .then((data) => {
-      let venue = {
-        venueId: data.venueId,
-        venueName: data.venueName,
-        lat: data.lat,
-        long: data.long,
-      };
-      res.status(200);
-      res.send(venue);
+    
+    app.get('/home', checkAuth, (req, res)=>{
+        res.send("Authenticated")
     })
-    .catch((err) => {
-      res.status(404);
-      res.send("Venue not found");
-    });
-});
-// get comments
-app.get("/comments/:venueId", (req, res) => {
-  res.setHeader("Content-Type", "application/json");
-  Comment.find({ venueId: req.params.venueId })
-    .then((data) => {
-      let comments = data.map((item, idx) => {
-        return { user: item.user, venueId: item.venueId, content: item.content };
+
+    app.get('/login', (req, res)=>{
+        res.status(403).send("login")
+    })
+
+    app.post("/login", (req, res, next) => {
+        passport.authenticate("local", (err, user, info) => {
+        console.log("running")
+        console.log("user", user)
+          if (err) throw err;
+          if (!user) res.send("Password or Username dont match");
+          else {
+            req.logIn(user, (err) => {
+              if (err) throw err;
+              res.status(200).send("Successfully Authenticated");
+              console.log(req.user);
+            });
+          }
+        })(req, res, next);
       });
-      res.status(200);
-      res.send(comments);
+
+    app.post('/register', async (req, res) => {
+        try {
+            const { values } = req.body
+            console.log("values>>", values)
+            const username = values.username ? values.username : ''
+            const email = values.email ? values.email : ''
+            const password = values.password ? await bcrypt.hash(values.password, 10) : ''
+            console.log("username>> ", username)
+            console.log("email>> ", email)
+            console.log("password>> ", password)
+
+            if (!username || !email || !password) {
+                return res.status(406).send("Field missing")
+            }
+
+            LoginModel.create({
+                username: username,
+                email: email,
+                password: password
+            })
+                .then(user => res.json(user))
+                .catch(err => res.json(err))
+
+
+
+        } catch (error) {
+
+        }
     })
-    .catch((err) => {
-      console.log(err);
-      res.status(406);
-      res.send(err);
-    });
-});
 
-//new comment
-app.post("/newcomment", (req, res) => {
-  res.setHeader("Content-Type", "text/plain");
-  Comment.create({
-    user: req.body.user,
-    venueId: req.body.venueId,
-    content: req.body.content,
-  })
-    .then(() => {
-      res.status(200);
-      res.send("New event created");
+    app.delete('/logout', (req, res, next) => {
+        req.logout(function(err) {
+            if (err) { 
+                return next(err); 
+            }
+            res.redirect('/login');
+        });
+    });
+    
+    app.get('/checkAuth', (req, res)=>{
+        res.set('Content-Type', 'application/json');
+        console.log("checking>>>")
+        if (req.isAuthenticated()){
+            console.log("checkAuth")
+            return res.status(200).send(req.user)
+        }
+        return res.status(403).send("Not Auth")
     })
-    .catch((err) => {
-      res.status(406);
-      res.send(err);
-    });
-});
 
-// handle ALL requests
-app.all("/*", (req, res) => {
-  //console.log(req);
-  // send this to client
-  res.send("Hello World!");
-});
+    function checkAuth (req, res, next) {
+        console.log("req.user>", req.user)
+        console.log("req.isAuthenticated())>>", req.isAuthenticated())
+        if (req.isAuthenticated()){
+            console.log("req.isAuthenticated>>", req.isAuthenticated)
+            return next()
+        }
+        console.log("redirectted")
+        res.redirect('/login')
+    }
 
-// listen to port 8000
-const server = app.listen(8000);
+})
+
+app.listen(3001, () => console.log("listening on port 3001"))
---
+++ src/signup.js
@@ -0,0 +1,118 @@
+import React, {useState} from 'react';
+import { Button, Checkbox, Form, Input, Typography  } from 'antd';
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
+const {Title} = Typography;
+
+const onFinishFailed = (errorInfo) => {
+    
+    console.log('Failed:', errorInfo);
+};
+const SignUp = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [pw, setPw] = useState('')
+    const navigate =useNavigate()
+    const onFinish = async (values) => {
+        
+        console.log('Success:', values);
+        try {
+            const postResult = await axios.post('http://localhost:3001/register', {values})
+            console.log("postResult>>", postResult)
+            if( postResult.status === 200){
+                navigate('/login')
+            }
+            
+        } catch (error) {
+            console.log("error>>", error)
+        }
+        
+    };
+    return (
+    <>
+    <Title level={2}>Register</Title>
+    <Form
+        name="basic"
+        labelCol={{
+            span: 8,
+        }}
+        wrapperCol={{
+            span: 16,
+        }}
+        style={{
+            maxWidth: 600,
+        }}
+        initialValues={{
+            remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+    >
+        <Form.Item
+            label="Username"
+            name="username"
+            rules={[
+                {
+                    required: true,
+                    message: 'Please input your username!',
+                },
+            ]}
+        >
+            <Input />
+        </Form.Item>
+
+        <Form.Item
+            label="email"
+            name="email"
+            rules={[
+                {
+                    required: true,
+                    message: 'Please input your Email!',
+                },
+            ]}
+        >
+            <Input />
+        </Form.Item>
+
+        <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+                {
+                    required: true,
+                    message: 'Please input your password!',
+                },
+            ]}
+        >
+            <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{
+                offset: 8,
+                span: 16,
+            }}
+        >
+            <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item
+            wrapperCol={{
+                offset: 8,
+                span: 16,
+            }}
+        >
+            <Button type="link" htmlType="submit">
+                Register
+            </Button>
+        </Form.Item>
+    </Form>
+    <Button type="link" htmlType="submit">
+        Login
+    </Button>
+    </>)
+};
+export default SignUp;
