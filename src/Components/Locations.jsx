600336812d9bc7d4991da16fe20bafec98768478
jeffreychl <jeffreyhlc@gmail.com>
2023-12-12 23:07:47 +0800

    Task1 & 3

--- src/Components/Locations.jsx
+++ src/Components/Locations.jsx
@@ -1,14 +1,53 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Table, Input } from 'antd';
+import { SearchOutlined} from '@ant-design/icons';
+import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
+
+const libraries = ["places"];
+
+function TempMap() {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: "AIzaSyBGGC2kgrhzogounenjJfsElrOkWmOFlM0",
+    libraries,
+  });
+  const mapContainerStyle = {
+    width: "100%",
+    height: "100%",
+  };
+  const center = {
+    lat: 22.302711,
+    lng: 114.177216,
+  };
+  if (loadError) {
+    console.log(loadError);
+    return <div>Error loading map</div>;
+  }
+  if (!isLoaded) {
+    return <div>Rendering</div>;
+  }
+  return (
+    <GoogleMap mapContainerStyle={mapContainerStyle} zoom={8} center={center}>
+      {/* {locationList && locationList.map((location, index) => 
+        // <MarkerF key={index} position={{ lat: location.lat, lng: location.lng }} />
+      )} */
+      <MarkerF position={center} />
+      }
+    </GoogleMap>
+  );
+}
+
 
 class Locations extends Component {
   constructor(props) {
     super(props);
     this.state = {
       locationList: [],
-      sortAscending: false,
+      filteredLocations: [],
     };
+
+    this.searchLocation = this.searchLocation.bind(this);
+
   }
 
   //invoked for each set state(only called once => new class + new componentdidmount)
@@ -19,124 +58,77 @@ class Locations extends Component {
   // load all locations in a table
   LoadLocationList() {
     axios({
-      // change the localhost to a public IP
-      url: "http://localhost:8000/venue",
-      method: "POST",
+        // need change localhost to the publicIP
+        url: "http://localhost:8000/venue",
+        method: "GET",
     })
-      .then((r) => {
+    .then((r) => {
+        console.log(r.data);
         this.setState({
-          locationList: r.data.map((venue) => ({
-            name: venue.venueName,
-            lat: venue.lat,
-            long: venue.long,
-            locid: venue.venueId,
-          })),
+            locationList: r.data,
+            filteredLocations: r.data
         });
-      })
-      .catch((err) => {
+    })
+    .catch((err) => {
         console.log(err);
-      });
+    });
   }
 
-  render() {
-    return (
-      <main>
-        <p>
-          <button id="sort-btn" onClick={() => this.sortTable()}>
-            Sort
-          </button>
-        </p>
-
-        <table id="locationTable">
-          <thead>
-            <tr>
-              <th>Location</th>
-              <th>Event Number</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.locationList.map((location, index) => (
-              <LocationRow
-                key={index}
-                i={index}
-                name={location.name}
-                programme={location.programme}
-                locid={location.locid}
-                loc_id={location._id}
-              />
-            ))}
-          </tbody>
-        </table>
-      </main>
-    );
-  }
-
-  // sorting of the table with the number of events at the venue
-  sortTable() {
-    var table, rows, switching, i, x, y, needToSwitch;
-    table = document.getElementById("locationTable");
-    switching = true; // a flag to track whether any switching has occured
-
-    // continue the loop until no more switching is needed
-    while (switching) {
-      switching = false;
-      rows = table.rows;
-
-      // loop through all table rows expect the header row
-      for (i = 1; i < rows.length - 1; i++) {
-        // Assume no switching is needed at the start of each iteration
-        needToSwitch = false;
-
-        // get the second cell (TD) from the current row and the next one that will be compared
-        x = rows[i].getElementsByTagName("TD")[1];
-        y = rows[i + 1].getElementsByTagName("TD")[1];
-
-        // compare the two cells based on whether sorting is ascending or descending
-        if (this.state.sortAscending) {
-          if (Number(x.innerHTML) > Number(y.innerHTML)) {
-            // if sorting is ascending and the current cell is greater than the next one, mark for switching and break the loop
-            needToSwitch = true;
-            break;
-          }
-        } else {
-          if (Number(x.innerHTML) < Number(y.innerHTML)) {
-            // if sorting is descending and the current cell is less than the next one, mark for switching and break the loop
-            needToSwitch = true;
-            break;
-          }
-        }
-      }
-
-      // If a switch is needed, perform the switch and mark that a switch has occurred
-      if (needToSwitch) {
-        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-        switching = true;
-      }
-    }
-
-    if (this.state.sortAscending) {
-      document.querySelector("#sort-btn").innerHTML = "Sort ascending";
-      this.setState({
-        sortAscending: false,
-      });
+  searchLocation(event) {
+    const filter = event.target.value.toLowerCase();
+  
+    if (filter === '') {
+      this.setState({ filteredLocations: this.state.locationList });
     } else {
-      document.querySelector("#sort-btn").innerHTML = "Sort descending";
-      this.setState({
-        sortAscending: true,
+      const filteredLocations = this.state.locationList.filter(location => {
+        // Assuming 'name' is the property you want to search in
+        const txtValue = Object.values(location).join(' ').toLowerCase();
+        return txtValue.indexOf(filter) > -1;
       });
+  
+      this.setState({ filteredLocations });
     }
   }
-}
 
-class LocationRow extends Component {
   render() {
+    const columns = [
+      {
+        title: 'Venue ID',
+        dataIndex: 'locid',
+        key: 'locid',
+        sorter: (a, b) => a.locid - b.locid,
+        sortDirections: ['descend', 'ascend'],
+      },
+      {
+        title: 'Venue Name',
+        dataIndex: 'name',
+        key: 'name',
+        render: (text, record) => <a href={`/venue/${record.locid}`}>{text}</a>,
+      },
+      {
+        title: 'Latitude',
+        dataIndex: 'lat',
+        key: 'lat',
+      },
+      {
+        title: 'Longitude',
+        dataIndex: 'long',
+        key: 'long',
+      },
+    ];
+
     return (
-      <tr>
-        <td id={"loc" + this.props.i}>
-          <Link to={`/dashboard/location/${this.props.locid}`}>{this.props.name}</Link>
-        </td>
-        <td>{this.props.programme.length}</td>
-      </tr>
+        <main>
+          <Input
+            size="large"
+            placeholder="Search"
+            onChange={this.searchLocation}
+            prefix={<SearchOutlined />}
+          />
+          <br />
+          <TempMap /> 
+          <Table columns={columns} dataSource={this.state.filteredLocations} rowKey="locid" />
+        </main>
     );
   }
 }
