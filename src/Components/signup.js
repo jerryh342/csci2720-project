fe202537242b45f2ade0f88852bd7d62ee54eb6a
Jerry Huen <jellybouncejerry@gmail.com>
2023-12-14 15:44:00 +0800

    exported signup form for use in user crud page

--- src/Components/signup.js
+++ src/Components/signup.js
@@ -3,19 +3,30 @@ import { Button, Checkbox, Form, Input, Typography, Tooltip, Space } from "antd"
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import NavBar from "./navbar";
-import ClockCircleOutlined from '@ant-design/icons/ClockCircleOutlined';
-import { useForm } from 'antd/lib/form/Form';
+import ClockCircleOutlined from "@ant-design/icons/ClockCircleOutlined";
+import { useForm } from "antd/lib/form/Form";
 const { Title } = Typography;
 
 const onFinishFailed = (errorInfo) => {
   console.log("Failed:", errorInfo);
 };
-const SignUp = () => {
+
+const RegisterPage = () => {
+  return (
+    <>
+      <NavBar />
+      <div>
+        <SignUp />
+      </div>
+    </>
+  );
+};
+export const SignUp = () => {
   const [form] = useForm();
   const [showErr, setShowErr] = useState(false);
-  const [userNameUsed, setUserNameUsed] = useState(false)
-  const [usernames, setUsernames] = useState([])
-  const [wrongPw, setWrongPw] = useState(false)
+  const [userNameUsed, setUserNameUsed] = useState(false);
+  const [usernames, setUsernames] = useState([]);
+  const [wrongPw, setWrongPw] = useState(false);
   const [formData, setFormData] = useState({
     username: "",
     password: "",
@@ -27,19 +38,18 @@ const SignUp = () => {
     setFormData({ ...formData, [event.target.name]: event.target.value });
   };
   const setError = (cb) => {
-    cb(true)
+    cb(true);
     form.resetFields();
-  }
-
+  };
 
   const handleSubmit = async (values) => {
-    console.log("submit")
-    console.log("values>>", formData)
-    if (formData.password !== formData.confirmPassword) setError(setShowErr)
-    if (!passwordRegex.test(formData.password)) setError(setWrongPw)
+    console.log("submit");
+    console.log("values>>", formData);
+    if (formData.password !== formData.confirmPassword) setError(setShowErr);
+    if (!passwordRegex.test(formData.password)) setError(setWrongPw);
     if (usernames.includes(formData.username)) {
       form.resetFields();
-      return setUserNameUsed(true)
+      return setUserNameUsed(true);
     }
 
     console.log("Success:", values);
@@ -55,30 +65,30 @@ const SignUp = () => {
   };
   useEffect(() => {
     const getUserNames = () => {
-      axios.get('http://localhost:8000/checkUsername')
-        .then(res => {
-          setUsernames(res.data)
+      axios
+        .get("http://localhost:8000/checkUsername")
+        .then((res) => {
+          setUsernames(res.data);
         })
-        .catch(err => console.log("err>>", err))
-    }
-    getUserNames()
-  }, [])
+        .catch((err) => console.log("err>>", err));
+    };
+    getUserNames();
+  }, []);
   return (
     <>
-      <NavBar />
       <div style={{ display: "flex", justifyContent: "center", marginTop: 100 }}>
         <Form
           form={form}
           name="basic"
           labelCol={{
-            span: 8
+            span: 8,
           }}
           wrapperCol={{
-            span: 16
+            span: 16,
           }}
           style={{
             width: "50%",
-            alignContent: "center"
+            alignContent: "center",
           }}
           initialValues={{
             remember: true,
@@ -89,58 +99,57 @@ const SignUp = () => {
         >
           <Form.Item
             labelCol={{
-              span: 8
+              span: 8,
             }}
             wrapperCol={{
-              span: 16
+              span: 16,
             }}
             label="username"
             name="username"
-            rules={[{ required: true, message: 'Username is required' }]}
-            style={{ color: "red", textAlign: 'left' }}
+            rules={[{ required: true, message: "Username is required" }]}
+            style={{ color: "red", textAlign: "left" }}
           >
-            <Input type="string"
+            <Input
+              type="string"
               name="username"
-              placeholder={userNameUsed ? "user exists"
-                : "input username"}
+              placeholder={userNameUsed ? "user exists" : "input username"}
               onChange={handleChange}
               status={userNameUsed ? "error" : ""}
               prefix={userNameUsed ? <ClockCircleOutlined /> : null}
-
             />
           </Form.Item>
-            <Form.Item
-              labelCol={{
-                span: 8
-              }}
-              wrapperCol={{
-                span: 16
-              }}
-              label="password"
-              name="password"
-              tooltip="Password must have at least 4 characters and one uppercase letter"
-              rules={[{ required: true, message: 'Password is required' }]}
-              style={{ color: "red", textAlign: 'left' }}
-            >
-              <Input.Password
-                placeholder={wrongPw ? "password not strong enough" : "input password"}
-                name="password"
-                onChange={handleChange}
-                status={wrongPw ? "error" : ""}
-                prefix={wrongPw ? <ClockCircleOutlined /> : null}
-              />
-            </Form.Item>
           <Form.Item
             labelCol={{
-              span: 8
+              span: 8,
             }}
             wrapperCol={{
-              span: 16
+              span: 16,
+            }}
+            label="password"
+            name="password"
+            tooltip="Password must have at least 4 characters and one uppercase letter"
+            rules={[{ required: true, message: "Password is required" }]}
+            style={{ color: "red", textAlign: "left" }}
+          >
+            <Input.Password
+              placeholder={wrongPw ? "password not strong enough" : "input password"}
+              name="password"
+              onChange={handleChange}
+              status={wrongPw ? "error" : ""}
+              prefix={wrongPw ? <ClockCircleOutlined /> : null}
+            />
+          </Form.Item>
+          <Form.Item
+            labelCol={{
+              span: 8,
+            }}
+            wrapperCol={{
+              span: 16,
             }}
             label="Confirm password"
             name="confirmPassword"
-            rules={[{ required: true, message: 'Password is required' }]}
-            style={{ color: "red", textAlign: 'left' }}
+            rules={[{ required: true, message: "Password is required" }]}
+            style={{ color: "red", textAlign: "left" }}
           >
             <Input.Password
               placeholder={showErr ? "password not match" : "confirm password"}
@@ -150,16 +159,16 @@ const SignUp = () => {
               prefix={showErr ? <ClockCircleOutlined /> : null}
             />
           </Form.Item>
-          <Button type="primary" htmlType="submit"
+          <Button
+            type="primary"
+            htmlType="submit"
             disabled={!formData.username || !formData.password || !formData.confirmPassword ? true : false}
           >
             Register
           </Button>
         </Form>
       </div>
-
-
     </>
   );
 };
-export default SignUp;
+export default RegisterPage;
