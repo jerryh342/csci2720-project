d161c937c82cd4b7c1ff2b9ca9e8e73d93e141b3
Jerry Huen <jellybouncejerry@gmail.com>
2023-12-15 01:22:36 +0800

    fixed editingdata undefined bug

--- src/Components/Event.jsx
+++ src/Components/Event.jsx
@@ -181,15 +181,14 @@ class Event extends Component {
 
   // Update an event
   updateEvent = (key) => {
-
     const updateData = key;
-        
+
     key = key.eventId;
 
     axios({
       url: `http://localhost:8000/admin/event/update/${key}`,
       method: "PUT",
-      data: editingValues,
+      data: this.state.editingValues,
     })
       .then((r) => {
         console.log(r.data);
@@ -205,155 +204,186 @@ class Event extends Component {
       });
   };
 
-    // Delete an event
-    deleteEvent = (e) => {    
-        axios({
-            // need change localhost to the publicIP
-            url: `http://localhost:8000/deleteevnet/${e}`, 
-            method: "DELETE",
-        })
-        .then((e) => {
-            this.setState({ isDelModalOpen: true });
-            this.loadEventList();
-        })
-        .catch((err) => console.log(err))
-    }
+  // Delete an event
+  deleteEvent = (e) => {
+    axios({
+      // need change localhost to the publicIP
+      url: `http://localhost:8000/deleteevnet/${e}`,
+      method: "DELETE",
+    })
+      .then((e) => {
+        this.setState({ isDelModalOpen: true });
+        this.loadEventList();
+      })
+      .catch((err) => console.log(err));
+  };
 
-    render(){
-        const { editingKey, editingValues } = this.state;
-        const columns = [
-            {
-                title: 'Event ID',
-                dataIndex: 'eventId',
-                key: 'eventId',
-            },
-            {
-                title: 'Event Name',
-                dataIndex: 'title',
-                key: 'title',
-            },
-            {
-                title: 'Venue',
-                dataIndex: 'loc',
-                key: 'loc',
-            },
-            {
-                title: 'Date Time',
-                dataIndex: 'date',
-                key: 'date',
-            },
-            {
-                title: 'Event Description',
-                dataIndex: 'desc',
-                key: 'desc',
-            },
-            {
-                title: 'Presenter',
-                dataIndex: 'presenter',
-                key: 'presenter',
-            },
-            {
-                title: 'Price',
-                dataIndex: 'price',
-                key: 'price',
-            },
-            {
-                title: "Operations",
-                key: "operations",
-                render: (text, record) => (
-                  <div>
-                    <Button style={{ marginBottom: "10px" }} type="primary" onClick={() => this.editEvent(record)}>
-                    Edit Event
-                    </Button>
-                    <Button type="primary" danger onClick={() => this.deleteEvent(record.name)}>
-                      Delete Event
-                    </Button>
-                  </div>
-                ),
-              },
-        ]
+  render() {
+    const { editingKey, editingValues } = this.state;
+    const columns = [
+      {
+        title: "Event ID",
+        dataIndex: "eventId",
+        key: "eventId",
+      },
+      {
+        title: "Event Name",
+        dataIndex: "title",
+        key: "title",
+      },
+      {
+        title: "Venue",
+        dataIndex: "loc",
+        key: "loc",
+      },
+      {
+        title: "Date Time",
+        dataIndex: "date",
+        key: "date",
+      },
+      {
+        title: "Event Description",
+        dataIndex: "desc",
+        key: "desc",
+      },
+      {
+        title: "Presenter",
+        dataIndex: "presenter",
+        key: "presenter",
+      },
+      {
+        title: "Price",
+        dataIndex: "price",
+        key: "price",
+      },
+      {
+        title: "Operations",
+        key: "operations",
+        render: (text, record) => (
+          <div>
+            <Button style={{ marginBottom: "10px" }} type="primary" onClick={() => this.editEvent(record)}>
+              Edit Event
+            </Button>
+            <Button type="primary" danger onClick={() => this.deleteEvent(record.name)}>
+              Delete Event
+            </Button>
+          </div>
+        ),
+      },
+    ];
 
-        return(
-            <main>
-                <div>
-                {/* <NavBar /> */}
-                <h1 style={{textAlign: "left"}}>Manage Users</h1>
-                </div>
-                {this.state.isButtonEnabled && (<Button style={{ marginLeft: "7px", backgroundColor: "pink", marginBottom: "10px" }} type="primary" onClick={() => this.setState({ allowInput: true, isButtonEnabled: false })}>Add Event</Button>)}
-                { <div style={{ clear: "both" }}>    
-                {this.state.allowInput && (
-                <Form onFinish={this.addEvent}>
-                    <Form.Item name="EventID" label="Event ID" rules={[{ required: true, message: "Please input the Event ID"}]}>
-                    <TextArea placeholder="Event ID" />
-                    </Form.Item>
-                    <Form.Item name="Title" label="Title" rules={[{ required: true, message: "Please input the title"}]}>
-                    <TextArea placeholder="Title" />
-                    </Form.Item>
-                    <Form.Item name="Venue" label="Venue" rules={[{ required: true, message: "Please input the venue"}]}>
-                    <TextArea placeholder="Location" />
-                    </Form.Item>
-                    <Form.Item name="Date" label="Date" rules={[{ required: true, message: "Please input the date"}]}>
-                    <TextArea placeholder="Date and Time" />
-                    </Form.Item>
-                    <Form.Item name="Description" label="Description" rules={[{ required: true, message: "Please input the description"}]}>
-                    <TextArea placeholder="Event Description" />
-                    </Form.Item>
-                    <Form.Item name="Presenter" label="Presenter" rules={[{ required: true, message: "Please input the presenter"}]}>
-                    <TextArea placeholder="Presenter" />
-                    </Form.Item>
-                    <Form.Item name="Price" label="Price" rules={[{ required: true, message: "Please input the price"}]}>
-                    <TextArea  placeholder="Price" />
-                    </Form.Item>
-                    <Form.Item>
-                    <Button 
-                        style={{marginBottom: '10px'}} 
-                        type="primary" 
-                        htmlType="submit" 
-                    >
-                    Submit
-                    </Button>
-                    </Form.Item>
-                </Form>
-                )}
-                </div>}
-                <div>
-                <Modal ref={this.formRef} title="Edit Event" open={this.state.isModalOpen} onOk={this.handleOk} onCancel={this.handleCancel}>
-                <Form ref={this.formRef} initialValues={{...this.state.editingValues, eventId: this.state.editingKey}} onFinish={this.updateEvent}>
-                    <Form.Item name="eventId" label="Event ID">
-                    <Input readOnly/>
-                    </Form.Item>
-                    <Form.Item name="title" label="Title" rules={[{ required: true, message: "Please input the title"}]}>
-                    <TextArea autoSize={{ minRows: 2, maxRows: 4 }}/>
-                    </Form.Item>
-                    <Form.Item name="loc" label="Venue" rules={[{ required: true, message: "Please input the venue"}]}>
-                    <TextArea autoSize={{ minRows: 2, maxRows: 4 }}/>
-                    </Form.Item>
-                    <Form.Item name="date" label="Date" rules={[{ required: true, message: "Please input the date"}]}>
-                    <TextArea autoSize={{ minRows: 2, maxRows: 4 }}/>
-                    </Form.Item>
-                    <Form.Item name="desc" label="Description" rules={[{ required: true, message: "Please input the description"}]}>
-                    <TextArea autoSize={{ minRows: 3, maxRows: 10 }}/>
-                    </Form.Item>
-                    <Form.Item name="presenter" label="Presenter" rules={[{ required: true, message: "Please input the presenter"}]}>
-                    <TextArea autoSize={{ minRows: 2, maxRows: 4 }}/>
-                    </Form.Item>
-                    <Form.Item name="price" label="Price" rules={[{ required: true, message: "Please input the price"}]}>
-                    <TextArea autoSize={{ minRows: 2, maxRows: 4 }}/>
-                    </Form.Item>
-                </Form>
-                </Modal>
-                <Modal 
-                    title="Delete Event" 
-                    open={this.state.isDelModalOpen}
-                    onOk={this.handleDelCancel}
+    return (
+      <main>
+        <div>
+          {/* <NavBar /> */}
+          <h1 style={{ textAlign: "left" }}>Manage Users</h1>
+        </div>
+        {this.state.isButtonEnabled && (
+          <Button
+            style={{ marginLeft: "7px", backgroundColor: "pink", marginBottom: "10px" }}
+            type="primary"
+            onClick={() => this.setState({ allowInput: true, isButtonEnabled: false })}
+          >
+            Add Event
+          </Button>
+        )}
+        {
+          <div style={{ clear: "both" }}>
+            {this.state.allowInput && (
+              <Form onFinish={this.addEvent}>
+                <Form.Item
+                  name="EventID"
+                  label="Event ID"
+                  rules={[{ required: true, message: "Please input the Event ID" }]}
                 >
-                    Successfully deleted!
-                </Modal>
-                <Table columns={columns} dataSource={this.state.eventList} rowKey="eventId" />
-                </div>
-            </main>
-        );
-    }
+                  <TextArea placeholder="Event ID" />
+                </Form.Item>
+                <Form.Item name="Title" label="Title" rules={[{ required: true, message: "Please input the title" }]}>
+                  <TextArea placeholder="Title" />
+                </Form.Item>
+                <Form.Item name="Venue" label="Venue" rules={[{ required: true, message: "Please input the venue" }]}>
+                  <TextArea placeholder="Location" />
+                </Form.Item>
+                <Form.Item name="Date" label="Date" rules={[{ required: true, message: "Please input the date" }]}>
+                  <TextArea placeholder="Date and Time" />
+                </Form.Item>
+                <Form.Item
+                  name="Description"
+                  label="Description"
+                  rules={[{ required: true, message: "Please input the description" }]}
+                >
+                  <TextArea placeholder="Event Description" />
+                </Form.Item>
+                <Form.Item
+                  name="Presenter"
+                  label="Presenter"
+                  rules={[{ required: true, message: "Please input the presenter" }]}
+                >
+                  <TextArea placeholder="Presenter" />
+                </Form.Item>
+                <Form.Item name="Price" label="Price" rules={[{ required: true, message: "Please input the price" }]}>
+                  <TextArea placeholder="Price" />
+                </Form.Item>
+                <Form.Item>
+                  <Button style={{ marginBottom: "10px" }} type="primary" htmlType="submit">
+                    Submit
+                  </Button>
+                </Form.Item>
+              </Form>
+            )}
+          </div>
+        }
+        <div>
+          <Modal
+            ref={this.formRef}
+            title="Edit Event"
+            open={this.state.isModalOpen}
+            onOk={this.handleOk}
+            onCancel={this.handleCancel}
+          >
+            <Form
+              ref={this.formRef}
+              initialValues={{ ...this.state.editingValues, eventId: this.state.editingKey }}
+              onFinish={this.updateEvent}
+            >
+              <Form.Item name="eventId" label="Event ID">
+                <Input readOnly />
+              </Form.Item>
+              <Form.Item name="title" label="Title" rules={[{ required: true, message: "Please input the title" }]}>
+                <TextArea autoSize={{ minRows: 2, maxRows: 4 }} />
+              </Form.Item>
+              <Form.Item name="loc" label="Venue" rules={[{ required: true, message: "Please input the venue" }]}>
+                <TextArea autoSize={{ minRows: 2, maxRows: 4 }} />
+              </Form.Item>
+              <Form.Item name="date" label="Date" rules={[{ required: true, message: "Please input the date" }]}>
+                <TextArea autoSize={{ minRows: 2, maxRows: 4 }} />
+              </Form.Item>
+              <Form.Item
+                name="desc"
+                label="Description"
+                rules={[{ required: true, message: "Please input the description" }]}
+              >
+                <TextArea autoSize={{ minRows: 3, maxRows: 10 }} />
+              </Form.Item>
+              <Form.Item
+                name="presenter"
+                label="Presenter"
+                rules={[{ required: true, message: "Please input the presenter" }]}
+              >
+                <TextArea autoSize={{ minRows: 2, maxRows: 4 }} />
+              </Form.Item>
+              <Form.Item name="price" label="Price" rules={[{ required: true, message: "Please input the price" }]}>
+                <TextArea autoSize={{ minRows: 2, maxRows: 4 }} />
+              </Form.Item>
+            </Form>
+          </Modal>
+          <Modal title="Delete Event" open={this.state.isDelModalOpen} onOk={this.handleDelCancel}>
+            Successfully deleted!
+          </Modal>
+          <Table columns={columns} dataSource={this.state.eventList} rowKey="eventId" />
+        </div>
+      </main>
+    );
+  }
 }
 export default Event;
-
