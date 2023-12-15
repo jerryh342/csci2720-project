4950a03e9c59bfca7b02f7ca0e4b1625cc4a5687
Jerry Huen <jellybouncejerry@gmail.com>
2023-12-15 14:49:01 +0800

    added option to display go console button

--- src/Components/Success.js
+++ src/Components/Success.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import { Button, Result } from 'antd';
-import { useNavigate } from "react-router-dom";const SuccessPage = (props) => {
-    const {status, title, subTitle, path} = props
-    const navigate = useNavigate();
+import React from "react";
+import { Button, Result } from "antd";
+import { useNavigate } from "react-router-dom";
 
-    return(
+const SuccessPage = (props) => {
+  const { status, title, subTitle, path, showButton } = props;
+  const navigate = useNavigate();
+
+  return (
     <>
-    <Result
+      <Result
         status={status}
         title={title}
         subTitle={subTitle}
         extra={[
-            <Button type="primary" key="console" onClick={()=>navigate(path)}>
-                Go Console
+          showButton ? (
+            <Button type="primary" key="console" onClick={() => navigate(path)}>
+              Go Console
             </Button>
+          ) : (
+            <></>
+          ),
         ]}
-    />
-    </>)
-}
+      />
+    </>
+  );
+};
 
-
+SuccessPage.defaultProps = {
+  showButton: true,
+};
 
 export default SuccessPage;
